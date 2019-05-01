2d20969333b6150a45ee3c01b800f334c8ae2506
Philip Diffenderfer <pdiffenderfer@gmail.com>
2019-04-30 21:27:52 -0400

    Extend interfaces in Vue.js

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 import _Vue, { WatchOptions } from 'vue';
 
 
+import './vue';
+
 
 export declare function install(Vue: typeof _Vue): void;
 
---
+++ vuex.d.ts
@@ -0,0 +1,26 @@
+/**
+ * Extends interfaces in Vue.js
+ */
+
+import Vue, { ComponentOptions } from 'vue';
+import { Store } from './index';
+
+declare module "vue/types/options" 
+{
+
+  interface ComponentOptions<V extends Vue> 
+  {
+    store?: Store<any>;
+  }
+
+}
+
+declare module "vue/types/vue" 
+{
+  
+  interface Vue 
+  {
+    $store: Store<any>;
+  }
+
+}
